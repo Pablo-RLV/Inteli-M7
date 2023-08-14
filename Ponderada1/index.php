4ed0029bf42263c1d854d81047a32c3ddfc6f39c
Pablo-Ruan <pablo.viana@sou.inteli.edu.br>
2023-08-13 22:32:46 -0300

    Final changes

---
+++ Ponderada1/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8"> 
+<style>
+  body{
+    margin: 0px;
+    padding: 0px;
+    background-image: radial-gradient(#e3ecf1 25%, #156e76 90%);
+    font-family: system-ui; 
+  }
+  .clearfix{
+    clear: both;
+  }
+  .main{ 
+    height: 1150px;
+    width: 800px;
+    background-color: white;
+    box-shadow:5px 7px 15px 5px #b9b6b6;
+    margin: 20px auto;
+  }
+  .top-selection{ 
+      background-color: #156e76;
+    text-align: center;
+    padding: 20px;
+  }
+  .profile{
+    width: 170px;
+    border-radius: 10%;
+    
+  }
+  .p1{
+    color: rgb(255, 255, 255);
+    font-size: 40px;
+    font-weight: bold;
+    margin: 0px;
+    margin-top: 10px
+  }
+  .texto{
+      margin: 50px;
+  }
+  .p1 span{ 
+    font-weight: 100;
+    color: rgb(53, 47, 232);
+    font-weight: bold;
+  }
+  .p2{ 
+    font-size: 20px;
+    color: #ffffff;
+    margin: 0px;
+    margin-top: 10px;
+  }
+  .accordion{
+      font-size: 1.17em;
+      font-weight: bold;
+  }
+</style>
+</head>
+<body> 
+    
+    <div class="main">
+        <div class = "top-selection">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDzl0UtH9D3MRD-FWaJ51yJmPhm_TxXpWFKuVITO8aRA&s" class="profile">
+            <p class="p1">Pablo Ruan</p>
+            <p class="p2">Estudante de Engenharia da Computação</p>
+        </div>
+        
+    <div class="texto">
+        <h3>Experiência profissional</h3>
+        <ul>
+            <li>Estágio no setor financeiro Inteli</li>
+        </ul>
+        <h3>Competências e habilidades</h3>
+        <ul>
+            <li>Liderança</li>
+            <li>Desenvolvimento de jogos</li>
+            <li>Programação</li>
+        </ul>
+        <details>
+            <summary class="accordion">Línguas</summary>
+            <ul>
+                <li>Português - fluente ou nativo</li>
+                <li>Inglês - avançado</li>
+                <li>Espanhol - intermediário</li>
+            </ul>
+        </details>
+        <h3>Certificados</h3>
+        <ul>
+            <li><a href="https://educa.s3.amazonaws.com/fia/certificados/25858-pablo-ruan-lana-viana-bootcamp.pdf">Bootcamp FIA Online</a></li>
+            <li><a href="https://www.conqueronline.com.br/certificates/2016bcf6-b27e-4b85-abbd-f528ab69dc9f">Produtividade e Gestão de Tempo</a></li>
+            <li><a href="https://www.primecursos.com.br/confirma/">Curso de Programação em Phyton</a></li>
+        </ul>
+        <p>
+        <center><a href = "https://www.linkedin.com/in/pablo-ruan-lana-viana-b0818b1a6/"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width = "50px"></a></center>
+        </p>
+                            
+                    </div>        
+                </div>
+            </div>
+        
+        </div> 
+    </div> 
+</body> 
+</meta> 
+</html> 
---
+++ Ponderada1/readme.md
@@ -0,0 +1,80 @@
+# Criando o ambiente para a execução de uma aplicação containerizada
+
+## 1. Instalação do Docker
+
+### 1.1. Instalação do Docker no Ubuntu 22.04 LTS
+
+#### 1.1.1. Atualização dos pacotes
+
+```bash
+sudo apt update
+```
+
+#### 1.1.2. Instalação dos pacotes necessários para a instalação do Docker
+
+```bash
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+```
+
+#### 1.1.3. Adição da chave GPG oficial do Docker
+
+```bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+```
+
+#### 1.1.4. Adição do repositório estável do Docker
+
+```bash
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+```
+
+#### 1.1.5. Atualização dos pacotes
+
+```bash
+sudo apt update
+```
+
+#### 1.1.6. Instalação do Docker
+
+```bash
+sudo apt install docker-ce
+```
+
+## 2. Estrutura do projeto
+
+### 2.1. Estrutura de diretórios
+
+```bash
+.
+├── index.php
+├── Dockerfile
+├── README.md
+```
+
+### 2.2. Descrição dos diretórios
+
+| Diretório | Descrição |
+| --- | --- |
+| index.php | Arquivo PHP que será executado pelo container |
+| Dockerfile | Arquivo que contém as instruções para a criação da imagem do container |
+| README.md | Arquivo que contém a documentação do projeto |
+
+## 3. Utilização da imagem do container
+
+O link para a imagem do container no Docker Hub é: <https://hub.docker.com/r/pablorlv/curriculo-php>
+
+### 3.1. Pull da imagem do container
+
+```bash
+docker pull pablorlv/curriculo-php:0.0.1
+```
+
+### 3.2. Execução da imagem do container
+
+```bash
+docker run -d -p 80:80 pablorlv/curriculo-php:0.0.1
+```
+
+## 4. Conclusão
+
+O projeto foi criado com sucesso e a imagem do container foi disponibilizada no Docker Hub, o que permite a qualquer pessoa acessar essa imagem independentemente da sua máquina. Logo, é possível perceber a importância da utilização de containers para a execução de aplicações, pois, além de facilitar a execução da aplicação, também facilita a disponibilização da aplicação para outras pessoas.
