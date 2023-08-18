618c2aa6e5a33cfed8b798e489b4d39c0efc5bbf
Pablo-Ruan <pablo.viana@sou.inteli.edu.br>
2023-08-18 15:00:01 -0300

    feat: style added

--- Ponderada2/app.py
+++ Ponderada2/app.py
@@ -1,7 +1,11 @@
 from flask import Flask,render_template,request,redirect, abort
+from flask_login import LoginManager
 from models import db,TextModel
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -11,7 +15,16 @@ db.init_app(app)
 def create_table():
     db.create_all()
 
-@app.route('/data/create' , methods = ['GET','POST'])
+@app.route('/login')
+def Login():
+    return render_template('index.html')
+
+@app.route('/')
+def RetrieveList():
+    infos = TextModel.query.all()
+    return render_template('read.html',infos = infos)
+
+@app.route('/create' , methods = ['GET','POST'])
 def create():
     if request.method == 'GET':
         return render_template('create.html')
@@ -21,16 +34,9 @@ def create():
         info = TextModel(texto=texto)
         db.session.add(info)
         db.session.commit()
-        return redirect('/data')
+        return redirect('/')
 
-
-@app.route('/data')
-def RetrieveList():
-    infos = TextModel.query.all()
-    return render_template('read.html',infos = infos)
-
-
-@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+@app.route('/update/<int:id>',methods = ['GET','POST'])
 def update(id):
     info = TextModel.query.filter_by(id=id).first()
     if request.method == 'POST':
@@ -41,20 +47,20 @@ def update(id):
             info = TextModel(texto=texto)
             db.session.add(info)
             db.session.commit()
-            return redirect(f'/data')
+            return redirect(f'/')
         return f"info with id = {id} Does nit exist"
 
     return render_template('update.html', info = info)
 
 
-@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+@app.route('/delete/<int:id>', methods=['GET','POST'])
 def delete(id):
     info = TextModel.query.filter_by(id=id).first()
     if request.method == 'POST':
         if info:
             db.session.delete(info)
             db.session.commit()
-            return redirect('/data')
+            return redirect('/')
         abort(404)
 
     return render_template('delete.html')
